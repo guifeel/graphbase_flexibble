9eab2fa076580a8c24878833f1485b2e23f35d2c
guifeel <guifeel@qq.com>
2023-07-31 15:44:21 +0800

    home展示

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>类别</h1>
+      <h1>发布</h1>
+      <h1>更多</h1>
+    </section>
+  );
 };
 
 export default Home;
